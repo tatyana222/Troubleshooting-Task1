948ba8341fde300bcc2f8e04d72577389ac962f9
tatyana222 <Tatyana_Gladysheva@epam.com>
2017-02-18 05:12:53 +0600

    Initial commit

---
+++ src/com/epam/troubleshooting/deadlock/Main.java
@@ -0,0 +1,42 @@
+package com.epam.troubleshooting.deadlock;
+
+public class Main {
+
+    private final static Object resource1 = new Object();
+    private final static Object resource2 = new Object();
+
+    public static void main(String[] args) {
+
+        Thread thread1 = new Thread() {
+            public void run() {
+                synchronized (resource1) {
+                    System.out.println("Thread 1 is locking resource 1");
+
+                    try { Thread.sleep(100);} catch (Exception e) {}
+
+                    synchronized (resource2) {
+                        System.out.println("Thread 1 is locking resource 2");
+                    }
+                }
+            }
+        };
+
+        Thread thread2 = new Thread() {
+            public void run() {
+                synchronized (resource2) {
+                    System.out.println("Thread 2 is locking resource 2");
+
+                    try { Thread.sleep(100);} catch (Exception e) {}
+
+                    synchronized (resource1) {
+                        System.out.println("Thread 2 is locking resource 1");
+                    }
+                }
+            }
+        };
+
+
+        thread1.start();
+        thread2.start();
+    }
+}
